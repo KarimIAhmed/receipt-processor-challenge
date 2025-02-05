32c8744ce285ecfe4c91f7fa7c6980ff41beede1
Karim Ahmed <karimib123@hotmail.com>
2025-02-04 22:24:41 -0500

    -Removed redundant dependencies -Removed unused imports

--- src/main/java/com/example/application/ReceiptsProcessor.java
+++ src/main/java/com/example/application/ReceiptsProcessor.java
@@ -1,15 +1,8 @@
 package com.example.application;
 
-import com.example.application.model.Item;
-import com.example.application.model.Receipt;
-import com.example.application.service.ReceiptService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
